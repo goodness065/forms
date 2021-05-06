55b64099e1aca540d88966eb8ae918afefb525a1
goodness065 <goodnesskelechi2017@yahoo.com>
2021-05-06 16:38:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let email =document.getElementById("email")
+
+// email.addEventListener('focus', function(){
+//     document.getElementById("form1").style.border = "1px solid red"
+// })
+
+// email.addEventListener('blur', function(){
+//     document.getElementById("form1").style.border = "1px solid rgb(194, 206, 206)"
+// })
+
